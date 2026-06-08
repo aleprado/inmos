608b328bea80ec791b1193566e18b865b4b293d1
Ale Prado <ale.nesti@gmail.com>
2026-06-07 21:15:25 -0300

    chore: disable retries in Cloud Tasks queue by setting max_attempts to 1

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -36,10 +36,7 @@ resource "google_cloud_tasks_queue" "whatsapp_queue" {
   }
 
   retry_config {
-    max_attempts  = 3
-    min_backoff   = "2s"
-    max_backoff   = "300s"
-    max_doublings = 16
+    max_attempts  = 1
   }
 
   depends_on = [google_project_service.services]
